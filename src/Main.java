1aa149740e04bc0894db700a52f816131fedf143
vladimir238 <bokovvladimir1@mail.ru>
2022-02-05 09:31:10 +0300

    Commit4

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         MakeArr arrWork = new MakeArr();
-        HourPayWork[] bigArr ;
+        HourPayWork[] bigArr;
         bigArr = arrWork.makeArr();
 
         for (int i = 0; i < 10; i++) {
-            System.out.println(bigArr[i].name + "   " + bigArr[i].ideWoker);
+            if (bigArr[i] != null) {
+                System.out.println(bigArr[i].name + "   " + bigArr[i].ideWoker);
+            }
         }
     }
 }
--- src/MakeArr.java
+++ src/MakeArr.java
@@ -1,15 +1,22 @@
+import java.util.Arrays;
+
 public class MakeArr {
+    String[] familyName={"Калиновер","Чаплыгина","Бабейчик","Бабенцов","Дернова","Дерменджи","Залюков","Калитвянский","Зальмунин","Чанчиков"};
+
+
     public HourPayWork[] makeArr(){
         HourPayWork[] arrWork = new HourPayWork[10];
         int n = 10;
-        for (int i = 0; i < arrWork.length; i++) {
-            String str = Integer.toString(i);
-            arrWork[i] = new HourPayWork(n + 10, str+"Петров", n + 100);
+        for (int i = 0; i < 5; i++) {
+        //    String str = Integer.toString(i);
+            arrWork[i] = new HourPayWork(n + 10, familyName[i], n + 100);
             n += 100;
         }
         return arrWork;
 
     }
+
+
 
 
 }
